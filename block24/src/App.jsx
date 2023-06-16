d0f539ec4081102a99cce4c7a672f78020f36c89
Maui Bautista <mherdy_2@hotmail.com>
2023-06-15 21:42:04 -0700

    adding css

--- block24/src/App.jsx
+++ block24/src/App.jsx
@@ -1,22 +1,34 @@
 import { useState } from "react";
 import { puppyList } from "./data.js";
+import "./App.css";
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
 
-  function handleClick() {
-    // some logic here
+  function handleClick(puppyId) {
+    setFeatPupId(puppyId);
   }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
     <div className="App">
-      {puppies.map((puppy) => {
-        return (
-          <p onClick={handleClick} key={puppy.id}>
-            {puppy.name}
-          </p>
-        );
-      })}
+      {puppies.map((puppy) => (
+        <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
+      ))}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
